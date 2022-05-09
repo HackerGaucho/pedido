30e7c50743cd9fbcf35d19b5daf7501a4d4ad0a3
Anderson Coutos <84669018+andercoutos@users.noreply.github.com>
2022-05-09 16:38:56 -0300

    M index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 $filename='resposta.txt';
 $str=file_get_contents($filename);
+$str=trim($str);
+if(is_numeric($str)){
+	$desde=date('r',$str);
 	require 'view/resposta.php';
+}else{
+	require 'view/pedido.php';
+}
 ?>
